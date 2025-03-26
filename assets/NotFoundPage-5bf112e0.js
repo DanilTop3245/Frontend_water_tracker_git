69c571d93bb343bfe69a5858971d43d348e6eb43
DanilTop3245 <turasov2009@gmail.com>
2025-03-26 21:04:25 +0200

    Updates

---
+++ assets/NotFoundPage-5bf112e0.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-224ccc6a.js";const e="/Frontend_water_tracker_git/assets/uG73t6y3S6obzxxr4w-66dc3bf5.gif",r=()=>t.jsx(t.Fragment,{children:t.jsx("img",{style:{display:"flex",alignItems:"center",margin:"auto",width:"500px",height:"auto"},src:e,alt:"NotFound"})});export{r as default};
